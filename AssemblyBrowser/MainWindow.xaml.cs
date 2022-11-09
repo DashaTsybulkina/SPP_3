dad8b0625b832cb752aac90d92a904f7c82d58b0
DashaTsybulkina <cybulkinadara@gmail.com>
2022-11-09 14:42:34 +0300

    view window

--- AssemblyBrowser/MainWindow.xaml.cs
+++ AssemblyBrowser/MainWindow.xaml.cs
@@ -15,14 +15,12 @@ using System.Windows.Shapes;
 
 namespace AssemblyBrowser
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = new MainWindowViewModel();
         }
     }
 }
